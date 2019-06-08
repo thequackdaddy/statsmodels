727d03fcb56989694aeafcea4201949f4fc1b0d8
thequackdaddy <pquack@gmail.com>
2019-06-08 17:46:42 -0500

    BLD: Cache more for mac homebrew

--- tools/ci/travis_pip.sh
+++ tools/ci/travis_pip.sh
@@ -23,7 +23,23 @@ fi
 # travis osx python support is limited. Use homebrew/pyenv to install python.
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
 
-  brew update -v && brew upgrade pyenv
+  if [ -d /usr/local/Homebrew/Library/Taps/caskroom/homebrew-cask ]; then
+    rm -rf /usr/local/Homebrew/Library/Taps/caskroom/homebrew-cask
+  fi
+
+  cd /usr/local/Homebrew
+
+  find . -name .git -type d -prune | while read d; do
+    cd $d/..;
+    git status;
+    git pull;
+    git clean -xdf;
+    cd $OLDPWD;
+  done
+
+  cd "$SRCDIR"
+
+  brew update && brew upgrade pyenv
 
   eval "$(pyenv init -)"
 
